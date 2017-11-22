72cdce4d9094955b34bcfa55f304aff8238be92f
iggna <33361773+iggna@users.noreply.github.com>
2017-11-22 20:59:17 -0300

    Add files via upload

---
+++ Nuevo/src/Nuevo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+public class Nuevo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //NewJFrame Formulario = new NewJFrame();
+        //Formulario.setVisible(true);
+        
+        Prueba pepe=new Prueba();
+        pepe.setVisible(true);
+    }
+
+}
